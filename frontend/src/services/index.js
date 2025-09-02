9ad7fad5a3cec9532cefb0be53569d40082e3f33
fadlyhts <fadlyhutasuhut1305@gmail.com>
2025-09-02 11:00:04 +0700

    feat: Add services index for API exports

---
+++ frontend/src/services/index.js
@@ -0,0 +1,10 @@
+// Export all services from this index file
+export * from './authService';
+export * from './lpService';
+export * from './sprindikService';
+export * from './beritaAcaraService';
+export * from './assetService';
+export * from './userService';
+
+// Export axiosConfig as api
+export { default as api } from './axiosConfig';
